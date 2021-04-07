e8393316616a01d8e185d428980d5c74ee841c8e
xiangli <421408812@qq.com>
2021-04-07 10:13:15 +0800

    add eslintrc auto pretty code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const someObj = {
+  a: 100,
+  b: 200,
+  eh: `${222}`
+};
 function App() {
+  useEffect(() => {
+    alert(devicePixelRatio);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +19,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>hello lolk at the 1px width;</li>
+          <li>hello lolk at the 2px width;</li>
+        </ul>
       </header>
+      <div style={{ border: '1px solid green', width: '100%', fontSize: 20 }} />
     </div>
   );
 }
